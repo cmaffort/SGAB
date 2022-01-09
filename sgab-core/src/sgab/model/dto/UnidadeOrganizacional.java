a75f886e1b3442550e2a2e52576236f08cdb60b2
Letícia Araújo <72109696+Baumora@users.noreply.github.com>
2022-01-08 23:54:39 -0300

    Update UnidadeOrganizacional.java

--- sgab-core/src/sgab/model/dto/UnidadeOrganizacional.java
+++ sgab-core/src/sgab/model/dto/UnidadeOrganizacional.java
@@ -1,19 +1,49 @@
+/*Author : @agatha
+  FR002 -> Gestão de Unidades Organizacionais;
+  FR002 -> Gestão de Unidades Organizacionais: Grupo D;
+  FR002 -> Gestão de Unidades Organizacionais Grupo D: @Letícia, @Agatha, @GabrielLucas & @MatheusGaston*/
+
+/*Get e Set rápido no NetBeans:
+  Selecione as variáveis, Refactor, Encapsulade Fields, Next, após isso, vai aparecer um local com o botão Do Refactoring. :idea:*/
 package sgab.model.dto;
 
-import sgab.model.dto.util.UnidadeOrganizacionalHelper;
-
-public class UnidadeOrganizacional {
+public class UnidadeOrganizacionalDTO {
+    private boolean habilitado = true;
+    private long id;
+    private String nome; 
+    private String endereco;
     
-    private Long id;
-    private String nome;
-    private String localizacao;
-
-    public UnidadeOrganizacional() {
-        this.id = UnidadeOrganizacionalHelper.getNextId();
+    //CRIAR -> constructors 
+    public UnidadeOrganizacionalDTO() {}
+    
+    public UnidadeOrganizacionalDTO(long id, String nome, String endereco) {
+        this.id = id;
+        this.nome = nome;
+        this.endereco = endereco;
+    }
+        
+    public UnidadeOrganizacionalDTO(boolean habilitado, long id, String nome, String endereco) {
+        this.habilitado = habilitado;
+        this.id = id;
+        this.nome = nome;
+        this.endereco = endereco;
     }
     
-    public Long getId() {
+    //EDITAR
+    public boolean getHabilitado() {
+        return habilitado;
+    }
+
+    public void setHabilitado(boolean habilitado) {
+        this.habilitado = habilitado;
+    }
+
+    public long getId() {
         return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 
     public String getNome() {
@@ -24,11 +54,20 @@ public class UnidadeOrganizacional {
         this.nome = nome;
     }
 
-    public String getLocalizacao() {
-        return localizacao;
+    public String getEndereco() {
+        return endereco;
     }
 
-    public void setLocalizacao(String localizacao) {
-        this.localizacao = localizacao;
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    
+    //IMPRIMIR
+    @Override
+    public String toString() {
+        return  "Habilitado = " + this.habilitado +
+                ", ID = " + this.id +
+                ", Nome = " + this.nome +
+                ", Endereço = " + this.endereco;
     }
 }
