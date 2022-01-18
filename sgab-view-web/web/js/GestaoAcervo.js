864d625dcfcec2c961d8a718dd4c2e47b866f415
Letícia Araújo <72109696+Baumora@users.noreply.github.com>
2022-01-18 14:03:07 -0300

    Add files via upload

---
+++ sgab-view-web/web/js/GestaoAcervo.js
@@ -0,0 +1,46 @@
+
+// Formulário de Envio
+function abreFormEnvio() {
+  document.getElementById("formEnvio").style.display = "block";
+}
+
+function fechaFormEnvio() {
+  document.getElementById("formEnvio").style.display = "none";
+}
+
+
+//Formulário de Retorno
+function abreFormRetorno() {
+  document.getElementById("formRetorno").style.display = "block";
+}
+
+function fechaFormRetorno() {
+  document.getElementById("formRetorno").style.display = "none";
+}
+
+//Formulário de Desativação
+function abreFormDesativar() {
+  document.getElementById("formDesativacao").style.display = "block";
+}
+
+function fechaFormDesativar() {
+  document.getElementById("formDesativacao").style.display = "none";
+}
+
+//Formulário de Consulta
+function abreFormConsulta() {
+  document.getElementById("formConsulta").style.display = "block";
+}
+
+function fechaFormConsulta() {
+  document.getElementById("formConsulta").style.display = "none";
+}
+
+//Formulário de Transferência
+function abreFormTransfer() {
+  document.getElementById("formTransfer").style.display = "block";
+}
+
+function fechaFormTransfer() {
+  document.getElementById("formTransfer").style.display = "none";
+}
